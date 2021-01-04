7f240e17124f0dc77e6f14f1849efb17f254025f
Greenvahn <saulverdemartinez@gmail.com>
2021-01-04 12:22:13 +0000

    Rename vue content component to mainContent

---
+++ src/components/mainContent.vue
@@ -0,0 +1,15 @@
+<template>
+  <main id="content" class="mx-auto max-w-full">
+    <div class="content-wrap">
+      <slot></slot>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Content blocks",
+};
+</script>
+
+<style></style>
