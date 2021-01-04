69d1d8a37b709cc5ebe8e558a7cf025610da4273
Greenvahn <saulverdemartinez@gmail.com>
2021-01-04 15:54:26 +0000

    vue footerIcon component + icons.json

---
+++ src/components/footer.vue
@@ -0,0 +1,56 @@
+<template>
+  <footer class="fixed-footer">
+    <p class="footNote">MADE BY SAUL VERDE / GREENSLASH</p>
+    <ul class="footer-links">
+      <li v-for="(item, index) in footerLinks" :key="index">
+        <a :href="item.link" :aria-label="item.name">
+          <!-- <img class="w-3.5 mt-5" :src="item.icon" /> -->
+          <FooterIcon :name="item.name" :width="25" :height="25" />          
+        </a>
+      </li>
+    </ul>
+  </footer>
+</template>
+
+<script>
+import FooterIcon from './footerIcon'
+export default {
+  name: "FootNote",
+  components: {
+    FooterIcon
+  },
+  setup() {
+    const footerLinks = [
+      {
+        name: "github",
+        link: "https://github.com/Greenvahn"
+      },
+      {
+        name: "linkedin",
+        link: "https://www.linkedin.com/in/saul-verde-martinez/"
+      },
+      {
+        name: "twitter",
+        link: "https://twitter.com/Greenvahn"
+      }
+    ];
+    return { footerLinks };
+  },
+};
+</script>
+
+<style scoped>
+.fixed-footer {
+  width: 100%;
+  left: 20px;
+  bottom: 20px;
+  position: fixed;
+}
+.footNote {
+  width: 100%;
+  font-size: 10px;
+  transform-origin: left;
+  transform: rotate(-90deg);
+  opacity: 0.4;
+}
+</style>
---
+++ src/components/footerIcon.vue
@@ -0,0 +1,39 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    class="icon mt-5"
+    viewBox="0 0 16 16"
+  >
+    <path :d="path()" />
+  </svg>
+</template>
+
+<script>
+import icons from "../assets/icons.json";
+
+export default {
+  name: "footerIcon",
+  props: {
+    name: String,
+    width: Number,
+    height: Number
+  },
+  setup(props){
+    const path = () => {
+      return icons[props.name];
+    }
+    return {path}
+  }
+};
+</script>
+
+<style scoped>
+.icon {
+  opacity: 0.5;
+}
+.icon:hover {
+  opacity: 1;
+}
+</style>
