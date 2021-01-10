afc5a7442a93de678e11f5de16e82bfe62074c4c
Greenvahn <saulverdemartinez@gmail.com>
2021-01-10 17:19:55 +0000

    GR-2 - About page - base structure

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,35 @@
 <template>
-  <div class="col-start-2 col-span-5">
-    <h1 class="text-3xl h-12">This is about template</h1>
-    <p class="max-w-xl">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </p>
+  <div class="wrapper-view max-w-3xl ">
+    <img
+      :src="require('../assets/Testimonials.png')"
+      class="inline-block col-span-1"
+    />
+    <div class="content col-span-2 space-y-4">
+      <h1 class="text-3xl h-12">This is home template</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+        nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+        volutpat.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+        nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+        volutpat. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+        diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+        volutpat.
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-name:'About'
-}
+  name: "About",
+};
 </script>
 
-<style>
-
+<style scoped>
+.wrapper-view {
+  @apply flex grid w-auto grid-cols-3 gap-2 ml-64 mr-32 pl-6;
+}
 </style>
