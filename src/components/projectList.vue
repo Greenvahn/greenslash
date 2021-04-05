73e2e98803950d78ff614f5360dc7d0dcacfab6c
Greenvahn <saulverdemartinez@gmail.com>
2021-04-05 18:11:42 +0100

    GR=38 - styles and media queries

--- src/components/projectList.vue
+++ src/components/projectList.vue
@@ -2,18 +2,18 @@
   <div
     v-for="(project, projectIndex) in datalist"
     :key="projectIndex"
-    class="flex flex-row col-span-3 text-sm p-3 bg-white text-black space-x-4 border border-gray-200 py-2"
+    class="flex flex-col col-span-3 text-sm p-3 bg-white text-black border border-gray-200 py-2 md:flex-row md:space-x-4"
   >
-    <h3 class="font-bold w-1/3">{{ t(`projectList.listItem.project${projectIndex}.id`) }}</h3>
-    <p v-for="(p, pIndex) in project.content" :key="pIndex" class="w-2/3">
+    <h3 class="font-bold w-full md:w-1/3">{{ t(`projectList.listItem.project${projectIndex}.id`) }}</h3>
+    <p v-for="(p, pIndex) in project.content" :key="pIndex" class="w-full md:w-2/3">
       {{ t(`projectList.listItem.project${projectIndex}.content.p${pIndex}`) }}
     </p>
-    <ul class="flex flex-row space-x-4 font-museomoderno w-1/3">
+    <ul class="flex flex-row space-x-4 font-museomoderno w-1/3 mt-5 text-gray-400 md:mt-0">
       <li v-for="(item, labelIndex) in project.labels" :key="labelIndex">
         {{ t(`projectList.listItem.project${projectIndex}.labels.name${labelIndex}`) }}
       </li>
     </ul>
-    <div id="links-box" class="relative flex justify-end mr-2 w-1/4">
+    <div id="links-box" class="absolute right-5 flex justify-end mr-2 w-1/4 md:relative md:right-0">
       <div
         v-for="(icon, inconIndex) in project.icons"
         :key="inconIndex"
